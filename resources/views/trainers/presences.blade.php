45a90d192ba2822d7a62c67e4f4343e22a127ffc
Michael Ravedoni <michael@ravedoni.com>
2023-11-05 21:45:20 +0100

    style: fix header

--- resources/views/trainers/presences.blade.php
+++ resources/views/trainers/presences.blade.php
@@ -4,7 +4,7 @@
         <section class="flex justify-between px-4 mx-auto max-w-screen-xl">
             <div class="relative overflow-x-auto w-full mx-auto">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3"> </th>
                             @foreach ($events as $event)
