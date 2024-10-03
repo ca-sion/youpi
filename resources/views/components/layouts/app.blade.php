a873e37e3fee3ef851fd66c998985633e0616da8
michaelravedoni <michael@ravedoni.com>
2024-10-03 20:35:58 +0200

    up: remove calendar and presence

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -54,10 +54,7 @@
                               <a href="{{ route('program') }}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Programmes</a>
                             </li>
                             <li>
-                              <a href="{{ route('events.index') }}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Calendrier</a>
-                            </li>
-                            <li>
-                              <a href="{{ route('trainers.presences') }}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Présences</a>
+                              <a href="https://tiiva.ch/casion/calendar" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Calendrier</a>
                             </li>
                         </ul>
                     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
                     </a>
                 </div>
                 <div class="grid mx-auto">
-                    <a href="{{ route('events.index') }}" class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <a href="https://tiiva.ch/casion/calendar" class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Calendrier</h2>
                     <p class="font-normal text-gray-700 dark:text-gray-400">Calendrier des compétitions et de la vie du club.</p>
                 </a>
