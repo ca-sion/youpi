0938b8fdbdc11d78aaa3e8dd1a97f39cc73263cc
michaelravedoni <michael@ravedoni.com>
2025-03-20 09:57:12 +0100

    up: navigation

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -49,6 +49,9 @@
                             <li>
                               <a href="{{ route('resources.list') }}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Ressources</a>
                             </li>
+                            <li>
+                              <a href="{{ route('documents.index') }}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Documents</a>
+                            </li>
                             @endif
                             <li>
                               <a href="{{ route('program') }}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Programmes</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,14 @@
                         <p class="font-normal text-gray-700 dark:text-gray-400">Tous les plans d'entraînements, le planification aisni que les resources nécessaires aux athlètes.</p>
                     </a>
                 </div>
+                @if (session()->get('protect_in'))
+                <div class="grid mx-auto">
+                    <a href="{{ route('documents.index') }}" class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Documents</h2>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">Tous les documents publics officiels du CA Sion à destination des entraîneurs et athlètes.</p>
+                    </a>
+                </div>
+                @endif
             </div>
         </section>
 
